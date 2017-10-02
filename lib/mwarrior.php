47be0aa8898e849302e985502d2d5416fbbc5f92
vaishsree <vaishsree@users.noreply.github.com>
2017-10-03 07:44:38 +1100

    Updated namespace

--- lib/mwarrior.php
+++ lib/mwarrior.php
@@ -1,11 +1,11 @@
 <?php
-if (!class_exists('\mwarrior\Settings')) {
-  require_once (__DIR__ . '/mwarrior/Settings.php');
-  require_once (__DIR__ . '/mwarrior/Logger.php');
-  require_once (__DIR__ . '/mwarrior/Response.php');
-  require_once (__DIR__ . '/mwarrior/GatewayCommand.php');
-  require_once (__DIR__ . '/mwarrior/Payment.php');
-  require_once (__DIR__ . '/mwarrior/Refund.php');
-  require_once (__DIR__ . '/mwarrior/AddCard.php');
-  require_once (__DIR__ . '/mwarrior/Webhook.php');}
+if (!class_exists('\primathonpay\Settings')) {
+  require_once (__DIR__ . '/primathonpay/Settings.php');
+  require_once (__DIR__ . '/primathonpay/Logger.php');
+  require_once (__DIR__ . '/primathonpay/Response.php');
+  require_once (__DIR__ . '/primathonpay/GatewayCommand.php');
+  require_once (__DIR__ . '/primathonpay/Payment.php');
+  require_once (__DIR__ . '/primathonpay/Refund.php');
+  require_once (__DIR__ . '/primathonpay/AddCard.php');
+  require_once (__DIR__ . '/primathonpay/Webhook.php');}
 ?>
