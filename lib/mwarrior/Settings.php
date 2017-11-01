e440d73835f271ceca83f431548b213a343824c3
vaishsree <vaishsree@users.noreply.github.com>
2017-11-01 17:00:18 +1100

    Added refund process

--- lib/mwarrior/Settings.php
+++ lib/mwarrior/Settings.php
@@ -1,13 +1,12 @@
 <?php
-
 namespace primathonpay;
 
-class Settings {
-  public static $merchantUUID = '';//merchantId
-  public static $apiKey = '';//apiKey
-  public static $apiPubKey = '';//apiPassPhrase
-  public static $gatewayBase  = 'https://base.merchantwarrior.com/token/';
-  public static $checkoutBase = 'https://base.merchantwarrior.com/token/';
-  public static $apiBase      = 'https://base.merchantwarrior.com/token/';
-}
-?>
+  class Settings {
+    public static $merchantUUID = '';//merchantId
+    public static $apiKey = '';//apiKey
+    public static $apiPubKey = '';//apiPassPhrase
+    public static $gatewayBase  = 'https://base.merchantwarrior.com/token/';
+    public static $checkoutBase = 'https://base.merchantwarrior.com/';
+    public static $apiBase      = 'https://base.merchantwarrior.com/token/';
+  }
+  ?>
